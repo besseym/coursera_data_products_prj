ceca42e5e0f4ff50fae4ae043d59fb258f8fffc4
Michael Bessey <besseym@gmail.com>
2015-12-21 08:43:38 -0500

    Initial commit

---
+++ start.R
@@ -0,0 +1,12 @@
+
+install.packages('shiny')
+
+library(UsingR)
+library(shiny)
+
+#http://rstudio.github.io/shiny/tutorial/
+
+
+setwd("/Users/michaelbessey/Education/Coursera/DataScience/09 Developing Data Products/coursera_data_products_prj")
+
+runApp()
---
+++ ui.R
@@ -0,0 +1,9 @@
+shinyUI(pageWithSidebar(
+  headerPanel("Coursera Data Products Project"),
+  sidebarPanel(
+    h3('Sidebar')
+  ),
+  mainPanel(
+    h3('Main Panel')
+  )
+))
