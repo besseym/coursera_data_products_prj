6109cc9a4a8a7aef496bedf3d80d59dec7959b03
Michael Bessey <besseym@gmail.com>
2015-12-26 11:56:24 -0500

    Complete project

--- server.R
+++ server.R
@@ -1,7 +1,22 @@
 
+data(GaltonFamilies)
+lm <- lm(childHeight ~ father + mother + gender, data = GaltonFamilies)
+
+predictChildHeight <- function(f, m, g){
+  
+  df_newdata <- data.frame(father = f, mother = m, gender = g)
+  predict(lm, newdata = df_newdata, interval = "prediction")
+}
+
 #Project Shiny Server
 shinyServer(
   function(input, output) {
     
+    output$fHeight <- renderText({input$fHeight})
+    output$mHeight <- renderText({input$mHeight})
+    output$cGender <- renderText({input$cGender})
+    
+    #prediction <- 
+    output$cHeight <- renderText({predictChildHeight(input$fHeight, input$mHeight, input$cGender)[1]})
   }
 )
--- ui.R
+++ ui.R
@@ -1,11 +1,27 @@
 
 #Project Shiny UI
 shinyUI(pageWithSidebar(
-  headerPanel("Coursera Data Products Project"),
+  headerPanel("Child Hight Prediction - A Coursera Data Products Project", windowTitle = "Child Hight Prediction"),
   sidebarPanel(
-    h3('Sidebar')
+    h3('Input'),
+    numericInput("fHeight", "Father's Height (inches)", 70, min = 1, max = 100),
+    numericInput("mHeight", "Mother's Height (inches)", 60, min = 1, max = 100),
+    selectInput(inputId = "cGender",
+                label = "Child's Gender",
+                choices = c("male", "female"),
+                selected = "male"),
+    submitButton("Calculate")
   ),
   mainPanel(
-    h3('Main Panel')
+    titlePanel("Results"),
+    h3("Input Recieved"),
+    h4("Father's Height (inches)"),
+    verbatimTextOutput("fHeight"),
+    h4("Mother's Height (inches)"),
+    verbatimTextOutput("mHeight"),
+    h4("Child's Gender"),
+    verbatimTextOutput("cGender"),
+    h3("Predicted Child's Height (inches)"),
+    verbatimTextOutput("cHeight")
   )
 ))
